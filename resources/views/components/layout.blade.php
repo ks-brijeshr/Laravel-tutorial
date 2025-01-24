56f9862eccaf873302c92c8b8268bcd51014bd69
brijesh rupavatiya <brijesh.rupavatiya@kalkani.in>
2025-01-24 16:50:33 +0530

    Video-5 Set the active navigation bar using @props and :active and when the user navigate home page the home icon will be lighter then about and contact in nav bar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,7 @@
     <title>home</title>
 </head>
 
-<body>
+<body class="h-full">
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -19,10 +19,10 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                {{-- :active="" che aema : colon che ae "/"" ne indicate kare ke ae expression che string nathi aetle ae jyare /contact avse tyare j ae page active avse nav ma bg color white thase --}}
+                                <x-nav-link href="/" :active="request()->is( '/')">Home</x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')" >About</x-nav-link>   
+                                <x-nav-link href="/contact" :active="request()->is('contact')" >Contact</x-nav-link> 
 
                             </div>
                         </div>
