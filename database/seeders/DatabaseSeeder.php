1fcd52051a314ca281899b5efe273ae57a5649c2
brijesh rupavatiya <brijesh.rupavatiya@kalkani.in>
2025-01-29 14:52:46 +0530

    Video-15 Databse-Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,13 +12,19 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
+    //run()is the by default and its call when  db:seed command execute
     public function run(): void
     {
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
         ]);
+
+        //if now we need to call job seeder from thia root seeder class then we give reference the class
+        $this->call(JobSeeder::class); // OR we can direclty writelike this Job::factory(200)->create() here in this file;
     }
 }
